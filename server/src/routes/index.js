f19c4f186b2a4988cdafcb841cfaf3c77dd3f519
Ashish <khatriaashish1@gmail.com>
2023-12-01 17:04:52 +0545

    Server Architecture

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,6 @@
 //imports
 const router = require('express').Router();
-const authRouter = require('../auth/auth.router')
+const authRouter = require('../app/auth/auth.router')
 
 //api
 router.use('/auth', authRouter);
