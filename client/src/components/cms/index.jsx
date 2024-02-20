a085c14f3e969163622c4d751856004d09676298
Ashish Khatri <60392989+Khatriaashish@users.noreply.github.com>
2024-02-20 14:46:34 +0545

    Delete client directory

--- client/src/components/cms/index.jsx
+++
@@ -1,9 +0,0 @@
-import TopHeaderComponent from "./top-header/top-header.component";
-import FooterComponent from "./footer/footer.component";
-import SidebarComponent from "./sidebar/sidebar.component";
-
-export { 
-    TopHeaderComponent,
-    FooterComponent,
-    SidebarComponent
-}
